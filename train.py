9b63eb46d8d2789a97d92f8ac2f12eca9ddb17ab
Quantum26 <iansong26@gmail.com>
2023-06-30 16:00:38 -0700

    renamed data_processing.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from gensim.models import Word2Vec
 import gensim.downloader
 from model.model_cnn import SentimentModel
-from data_funcs import data_generator
+from data_processing import data_generator
 
 print("loading Word2Vec....")
 model = gensim.downloader.load('glove-wiki-gigaword-50')
