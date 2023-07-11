b6200093c6e174dbd4e9424270ea3763ef220e4b
Quantum26 <iansong26@gmail.com>
2023-07-10 23:39:34 -0700

    started training script

--- train.py
+++ train.py
@@ -1,9 +1,38 @@
 from gensim.models import Word2Vec
-import gensim.downloader
+from word2vec import load_word2vec
 from models.model_cnn import SentimentModel
+import torch.nn as nn
 from data_processing import data_generator
 
+import torch.optim as optim
+
 print("loading Word2Vec....")
-model = gensim.downloader.load('glove-wiki-gigaword-50')
+w2vmodel = load_word2vec()
 print("Word2Vec loaded!")
 
+model = SentimentModel(w2vmodel)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+running_loss = 0.0
+for i, data in enumerate(data_generator()):
+    # get the inputs; data is a list of [inputs, labels]
+    inputs, labels = data
+
+    # zero the parameter gradients
+    optimizer.zero_grad()
+
+    # forward + backward + optimize
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+
+    # print statistics
+    running_loss += loss.item()
+    if i % 2000 == 1999:    # print every 2000 mini-batches
+        print(f'[{1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+        running_loss = 0.0
+
+print('Finished Training')
